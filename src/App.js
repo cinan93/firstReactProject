eb3093a867cc418b11d3a4aa8763202d7e0d27ba
cinan93 <can.inan@bluewin.ch>
2020-02-12 15:50:14 +0100

    some commits

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Hi I am a react App</h1>
-    </div>
-  );
+  //return (
+    //<div className="App">
+    //<h1>Hi I am a react App</h1>
+    //</div>
+//);
+return React.createElement('div', null, 'Hi I am a react App'); 
 }
 
 export default App;

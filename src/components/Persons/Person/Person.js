1222becdca1ed0c132ec9616f976fcb96b587c05
Can Inan <Can_Inan@cans-imac.home>
2020-03-16 20:44:46 +0100

    finish

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
+
+import Aux from '../../../hoc/Aux'
+import classes from './Person.module.css';
+import withClass from '../../../hoc/withClass';
+
+class Person extends Component {
+
+  constructor(props){
+    super(props); 
+    this.inputElementRef = React.createRef(); 
+  }
+
+  componentDidMount(){
+    //this.inputElement.focus();
+    this.inputElementRef.current.focus(); 
+  }
+
+
+  render(){
+    console.log('[Person.js] rendering...')
+    return(
+      <Aux>
+        {this.props.isAuth ? <p>Authenticated!</p> : <p>Please Log in</p>}
+        <p onClick={this.props.click}> I'm {this.props.name} an I am  {this.props.age} years old</p>   
+        <p>{this.props.children}</p>
+        <input 
+        //ref={(inputEl) => {this.inputElement = inputEl}}
+        ref= {this.inputElementRef}
+        type="text" onChange={this.props.changed} value={this.props.name}/>
+      </Aux>
+            );
+    }
+  }
+
+Person.propTypes = {
+  click: PropTypes.func, 
+  name: PropTypes.string, 
+  age: PropTypes.number,
+  changed: PropTypes.func
+}; 
+
+export default withClass(Person, classes.Person);
---
+++ src/containers/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from '../hoc/withClass'; 
+import Aux from '../hoc/Aux'; 
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor'); 
+  }
+
+  state = {
+    persons: [
+      {id : 'asdfsd', name: 'Can', age: 26},
+      {id : 'kjklkj', name: 'Kyle', age: 23},
+      {id : 'uejdj', name: 'Nico',age: 30}
+    ],
+    otherState:'some other value', 
+    showPersons: false, 
+    showCockpit: true,
+    changeCounter: 0, 
+    authenticated:false
+  }
+
+  static getDerivedStateFromProps(props,state){ 
+    console.log('[App.js] getDerivedStateFromProps',props); 
+    return state; 
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount'); 
+  }
+
+  shouldComponentUpdate(nextProps,nextState){
+    console.log('[App.js] shouldComponentUpdate')
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate')
+  }
+
+ deletePersonHandler = (personIndex)=>{
+   // const persons = this.state.persons.slice(); 
+   const persons =[...this.state.persons]; 
+   persons.splice(personIndex,1); 
+   this.setState({persons:persons}); 
+ }
+  
+  nameChangedHandler = (event,id) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id; 
+    }) ; 
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value; 
+    
+    const persons =[...this.state.persons]; 
+    persons[personIndex] = person; 
+
+    this.setState((prevState, props) => { 
+      return { 
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1 
+      };
+    }); 
+  }; 
+
+  togglePersonsHandler = () => {
+  const doesShow = this.state.showPersons; 
+  this.setState({showPersons: !doesShow}); 
+  }
+
+  loginHandler = () => { 
+    this.setState({authenticated: true})
+
+  }
+
+  render(){
+    console.log('[App.js] render'); 
+    let persons = null; 
+
+    if(this.state.showPersons === true){
+      persons = <Persons 
+          persons={this.state.persons}
+          clicked ={this.deletePersonHandler}
+          changed = {this.nameChangedHandler}
+          isAuthenticated= {this.state.authenticated}/>
+      }
+
+    return (
+        <Aux>
+        <button 
+          onClick = {()=>{ 
+            this.setState({showCockpit: false}); }}> 
+            Remove Cockpit </button>
+          {this.state.showCockpit ? <Cockpit
+          title ={this.props.appTitle}
+          showPersons={this.state.showPersons} 
+          personsLength={this.state.persons.length} 
+          clicked ={this.togglePersonsHandler}
+          login={this.loginHandler}/> : null}
+          {persons}
+       </Aux>
+        )     
+}
+    }
+
+export default withClass(App, classes.App);
